a18d2503750cd2b952c0a4222869502610fbf6ab
Elle H <itsellej@gmail.com>
2018-08-04 15:44:31 +0100

    Add output for win, lose, draw

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ let userScore = 0;
 let computerScore = 0;
 
 const userScoreSpan = document.getElementById("user-score");
-const userComputerSpan = document.getElementById("computer-score");
+const computerScoreSpan = document.getElementById("computer-score");
 const scoresDiv = document.querySelector(".scores");
-const resultDiv = document.querySelector(".result");
+const resultDiv = document.querySelector(".result > p");
+const totalP = document.querySelector("total");
 
 const rockDiv = document.getElementById("rock");
 const paperDiv = document.getElementById("paper");
@@ -13,50 +14,101 @@ const scissorsDiv = document.getElementById("scissors");
 
 // Generating computer choice at random
 const getComputerChoice = () => {
-  const choiceList = ["rock", "paper", "scissors"]
+  const choiceList = ["rock", "paper", "scissors"];
   const randomNumber = Math.floor(Math.random()*3);
   return (choiceList[randomNumber]);
+};
+
+
+// Convert lowercase choice to uppercase
+const convertToUp = (word) => {
+  switch(word) {
+    case "rock":
+      return "Rock";
+      break;
+    case "paper":
+      return "Paper";
+      break;
+    case "scissors":
+      return "Scissors"
+      break;
+  }
+};
+
+
+// Output for user win
+const win = (userChoice, computerChoice) => {
+  userScore++;
+  userScoreSpan.innerHTML = userScore;
+  const randomWin = ["beats", "smashes", "destroys", "obliterates"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  resultDiv.innerHTML = `${convertToUp(userChoice)} ${randomWin[randomNumber]} ${convertToUp(computerChoice)}. You win!`;
+};
+
+// Output for user lose / computer win
+const lose = (userChoice, computerChoice) => {
+  computerScore++
+  computerScoreSpan.innerHTML = computerScore;
+  const randomWin = ["beats", "smashes", "destroys", "obliterates"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  resultDiv.innerHTML = `${convertToUp(computerChoice)} ${randomWin[randomNumber]} ${convertToUp(userChoice)}. You lose!`;
+
+};
+
+// Output for tie
+const tie = (userChoice, computerChoice) => {
+  resultDiv.innerHTML = `${convertToUp(computerChoice)} matches ${convertToUp(userChoice)}. It's a tie!`;
 }
+
+//Output for total
+
+
+
+
 
 
 // Comparing computerChoice and userChoice
 const game = (userChoice) => {
   const computerChoice = getComputerChoice();
 
-  switch(userChoice + computerchoice) {
+  // console.log(userChoice)
+  // console.log(computerChoice)
+
+  switch (userChoice + computerChoice) {
     case "paperrock":
     case "rockscissors":
     case "scissorspaper":
-      console.log("USER WINS")
+      win(userChoice, computerChoice);
       break;
 
     case "rockpaper":
     case "scissorsrock":
     case "paperscissors":
-      console.log("COMPUTER WINS")
+      lose(userChoice, computerChoice);
       break;
 
     case "rockrock":
     case "paperpaper":
     case "scissorsscissors":
-      console.log("TIE")
+      tie(userChoice, computerChoice);
       break;
   }
 
-}
+};
 
 const main = () => {
   rockDiv.addEventListener('click', function()  {
-    game("rock")
-  })
+    game("rock");
+  });
 
   paperDiv.addEventListener('click', function(){
     game("paper");
-  })
+  });
 
   scissorsDiv.addEventListener('click', function(){
     game("scissors");
-  })
-}
+  });
+};
 
-game();
+
+main();
