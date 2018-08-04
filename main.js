cbae05e27a3b66dd23480808a3fed9db7235c02a
Elle H <itsellej@gmail.com>
2018-08-04 13:32:17 +0100

    Add variables

---
+++ main.js
@@ -0,0 +1,11 @@
+let userScore = 0;
+let computerScore = 0;
+
+const userScoreSpan = document.getElementById('#user-score');
+const userScoreSpan = document.getElementById('#computer-score');
+const scoresDiv = document.queryselector(".scores");
+const resultDiv = document.queryselector(".result")
+
+const rockDiv = document.getElementById("rock")
+const paperDiv = document.getElementById("paper")
+const scissorsDiv = document.getElementById("scissors")
