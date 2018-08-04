200b19dfb33dd9ae1eeb09b6c5b5a03dcbb85644
Elle H <itsellej@gmail.com>
2018-08-04 13:55:53 +0100

    Add event listeners, game and main function

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
 let userScore = 0;
 let computerScore = 0;
 
-const userScoreSpan = document.getElementById('#user-score');
-const userScoreSpan = document.getElementById('#computer-score');
-const scoresDiv = document.queryselector(".scores");
-const resultDiv = document.queryselector(".result")
+const userScoreSpan = document.getElementById("user-score");
+const userComputerSpan = document.getElementById("computer-score");
+const scoresDiv = document.querySelector(".scores");
+const resultDiv = document.querySelector(".result");
 
-const rockDiv = document.getElementById("rock")
-const paperDiv = document.getElementById("paper")
-const scissorsDiv = document.getElementById("scissors")
+const rockDiv = document.getElementById("rock");
+const paperDiv = document.getElementById("paper");
+const scissorsDiv = document.getElementById("scissors");
+
+const game = (userChoice) => {
+  console.log("poop" + userChoice);
+}
+
+const main = () => {
+  rockDiv.addEventListener('click', function()  {
+    game("rock")
+  })
+
+  paperDiv.addEventListener('click', function(){
+    game("paper");
+  })
+
+  scissorsDiv.addEventListener('click', function(){
+    game("scissors");
+  })
+}
+
+main();
